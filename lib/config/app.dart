6b8959edbf12f5d75e33f139aa0a6e2286866acc
sdf0106 <d.sagatov@student.inha.uz>
2022-05-30 14:16:45 +0500

    Initial commit with configuration of the App -1

---
+++ lib/config/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QyreTestApp extends StatelessWidget {
+  const QyreTestApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
