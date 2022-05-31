cce17669442ab8b725273e7f9476c694db8e6b4a
sdf0106 <d.sagatov@student.inha.uz>
2022-05-31 16:23:33 +0500

    fix(availability): injection fixed

--- lib/features/availability/domain/repository/available_days_repository.dart
+++ lib/features/availability/domain/repository/available_days_repository.dart
@@ -1,10 +1,8 @@
 import 'package:dartz/dartz.dart';
-import 'package:injectable/injectable.dart';
 
 import '../../../../core/errors/failures.dart';
 import '../entities/day.dart';
 
-@Injectable()
 abstract class AvailableDaysRepository {
   Future<Either<Failure, List<Day>>> getAvailableDays();
 }
